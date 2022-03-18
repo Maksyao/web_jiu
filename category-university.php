e81e5d8b45000e993569fad7d493ba881360dd80
Maxim Petrishin <petrishinmax@gmail.com>
2022-03-18 22:54:09 +0300

    Uni-cards changes

--- category-university.php
+++ category-university.php
@@ -8,10 +8,12 @@ get_header(); ?>
     ?>
         <a href="<?php the_permalink(); ?>" class="uni-card-a">
             <div class="uni-card">
-                <img src="<?= CFS()->get('uni_logo'); ?>" alt="logo" class="uni-logo">
+                <div class="uni-logo">
+                    <img src="<?= CFS()->get('uni_logo'); ?>" alt="logo" class="uni-logo-img">
+                </div>
+                
                 <div class="uni-info">
                     <h3 class="uni-name"><?= the_title();?></h3>
-                    <p class="uni-text"><?= the_title();?></p>
                 </div>
             </div>
         </a>
